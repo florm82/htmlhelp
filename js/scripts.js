57c95e72c000815c39f47676fc90585e183cf335
Flor Nazari <florm82@gmail.com>
2019-12-14 17:21:44 -0800

    add some functions

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, This is a header.");
+  });
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
